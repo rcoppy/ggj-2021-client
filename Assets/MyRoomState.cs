500a942f575bacf7f41254a646b2c8cf0837ee20
Alexander Rupp-Coppi <amr2311@columbia.edu>
2021-01-29 08:00:38 -0500

    debugging state serialization

---
+++ Assets/MyRoomState.cs
@@ -0,0 +1,15 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 1.0.10
+// 
+
+using Colyseus.Schema;
+
+// namespace Dossamer.Ggj2021 {
+	public partial class MyRoomState : Schema {
+		[Type(0, "string")]
+		public string mySynchronizedProperty = default(string);
+	}
+// }
